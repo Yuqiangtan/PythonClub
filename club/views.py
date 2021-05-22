5ceeccac8c88684da298b3da049859008e3d2f37
Yuqiangtan <56171017+Yuqiangtan@users.noreply.github.com>
2021-05-22 14:49:38 +0800

    Add files via upload

--- club/views.py
+++ club/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,11 @@ def index(request):
 def resources(request):
     resource_list=Resource.objects.all()
     return render(request,'club/resources.html',{'resource_list':resource_list})
+
+def meeting(request):
+    meeting_list=Meeting.objects.all()
+    return render(request,'club/meeting.html',{'meeting_list':meeting_list})
+
+def meetingDetail(request,id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request,'club/meetingdetail.html',{'meeting':meeting})
